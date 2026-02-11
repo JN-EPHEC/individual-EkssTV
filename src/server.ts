2ce453324fc749fba690c396c2f643e66f162eb1
EkssTV <matthieu.decreme@gmail.com>
2026-02-11 17:38:43 +0100

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
-
+import express from 'express';
 
 function greet(name: string): string {
     return 'Salut ' + name ;
-}
+};
 
-let message = greet('Yuqi')
-console.log(message)
+let message = greet('Yuqi');
+console.log(message);
 
+const app = express();
+const port = 3000;
 
+app.get('/',(req,res) => {
+    res.send('Salut c\'est jhonny Bienvenu sur mon API');
+    console.log(typeof(req),typeof(res));
+});
+
+app.listen(port, () => {
+    console.log(`Serveur lancé sur http://localhost:${port}`);
+});
